d50019f5f03b72a65bd75c9cae891bc3c5b88e12
DESKTOP-9402VFR\Juan Ramon Canto <juanr.canto@gmail.com>
2022-11-10 12:14:45 +0100

    Inicio 10112022

--- CSPharma_TorreControl/Program.cs
+++ CSPharma_TorreControl/Program.cs
@@ -1,7 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//Añadimos los servicios que nos interesa al constructor
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<entityBasicoContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("EFCConexion"))
+    );
 
 var app = builder.Build();
 
