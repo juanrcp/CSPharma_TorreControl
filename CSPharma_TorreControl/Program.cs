7f8aff75493bff6cb5bcf7bb3393e37a363f301b
JUAN RAMON <juanr.canto@gmail.com>
2022-11-10 19:47:52 +0100

    Migracion y clase AccesoViewModel realizada e inyectada en el index 10112022

--- CSPharma_TorreControl/Program.cs
+++ CSPharma_TorreControl/Program.cs
@@ -1,11 +1,9 @@
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+
 var builder = WebApplication.CreateBuilder(args);
 
-//Añadimos los servicios que nos interesa al constructor
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<entityBasicoContext>(
-    o => o.UseNpgsql(builder.Configuration.GetConnectionString("EFCConexion"))
-    );
 
 var app = builder.Build();
 
---
+++ EntityBasicoDAL/cspharma_informacional/TdcCatEstadosEnvioPedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityBasicoDAL.cspharma_informacional;
+
+public partial class TdcCatEstadosEnvioPedido
+{
+    public int Id { get; set; }
+
+    public string CodEstadoEnvio { get; set; } = null!;
+
+    public string? DesEstadoEnvio { get; set; }
+
+    public DateTime MdDate { get; set; }
+
+    public string MdUuid { get; set; } = null!;
+
+    public virtual ICollection<TdcTchEstadoPedido> TdcTchEstadoPedidos { get; } = new List<TdcTchEstadoPedido>();
+}
