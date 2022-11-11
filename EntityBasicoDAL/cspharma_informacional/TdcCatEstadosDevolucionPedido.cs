0107a4e1071e86f673764973706c0af9a1477a86
DESKTOP-9402VFR\Juan Ramon Canto <juanr.canto@gmail.com>
2022-11-11 09:46:25 +0100

    Terminado con vista hecha 11112022

---
+++ EntityBasicoDAL/cspharma_informacional/TdcCatEstadosDevolucionPedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityBasicoDAL.cspharma_informacional;
+
+public partial class TdcCatEstadosDevolucionPedido
+{
+    public int Id { get; set; }
+
+    public string CodEstadoDevolucion { get; set; } = null!;
+
+    public string? DesEstadoDevolucion { get; set; }
+
+    public DateTime MdDate { get; set; }
+
+    public string MdUuid { get; set; } = null!;
+
+    public virtual ICollection<TdcTchEstadoPedido> TdcTchEstadoPedidos { get; } = new List<TdcTchEstadoPedido>();
+}
