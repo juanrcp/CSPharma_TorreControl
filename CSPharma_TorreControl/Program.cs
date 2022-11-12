0936300762e3db7fb71e6cb9e7db30a688788005
JUAN RAMON <juanr.canto@gmail.com>
2022-11-12 17:28:54 +0100

    Corrercion de error en subida a Git 12112022

--- CSPharma_TorreControl/Program.cs
+++ CSPharma_TorreControl/Program.cs
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-=======
 using EntityBasicoDAL.cspharma_informacional;
 using Microsoft.EntityFrameworkCore;
->>>>>>> e5de13ec8660ebe848d93f77819343ba0ceedf23
 using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
 
 var builder = WebApplication.CreateBuilder(args);
 
-<<<<<<< HEAD
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-=======
+
 //Esto lo añadimos para que reconozla los @. 
 builder.Services.AddRazorPages();
 
@@ -23,7 +19,7 @@ builder.Services.AddEntityFrameworkNpgsql()
     {
         options.UseNpgsql(builder.Configuration.GetConnectionString("EFCConexion"));
     });
->>>>>>> e5de13ec8660ebe848d93f77819343ba0ceedf23
+
 
 var app = builder.Build();
 
